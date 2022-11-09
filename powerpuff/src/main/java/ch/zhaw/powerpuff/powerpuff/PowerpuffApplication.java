83f52df395699bfff03f05f8025342c410e32284
kollesal <kollesal@students.zhaw.ch>
2022-11-09 18:23:45 +0100

    Add files via upload

---
+++ powerpuff/src/main/java/ch/zhaw/powerpuff/powerpuff/PowerpuffApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.powerpuff.powerpuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerpuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerpuffApplication.class, args);
+	}
+
+}
---
+++ powerpuff/src/test/java/ch/zhaw/powerpuff/powerpuff/PowerpuffApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.powerpuff.powerpuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerpuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
